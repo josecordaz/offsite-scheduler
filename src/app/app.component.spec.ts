0c97ac8a8561d0b864bdd5b6281117f8ad60ce63
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2018-05-15 21:05:06 -0500

    fixing issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-=======
 
 import { AppComponent } from './app.component';
 
->>>>>>> 3136ed6838b3d5100829a59b12fce5f00593cbef
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,22 +10,11 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-<<<<<<< HEAD
-=======
-
->>>>>>> 3136ed6838b3d5100829a59b12fce5f00593cbef
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-<<<<<<< HEAD
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-=======
 
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -37,15 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('app works!');
   }));
 
->>>>>>> 3136ed6838b3d5100829a59b12fce5f00593cbef
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-<<<<<<< HEAD
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-=======
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
->>>>>>> 3136ed6838b3d5100829a59b12fce5f00593cbef
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-=======
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
->>>>>>> 3136ed6838b3d5100829a59b12fce5f00593cbef
 
 import { AppComponent } from './app.component';
 
@@ -15,14 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-<<<<<<< HEAD
-    BrowserModule
-=======
     BrowserModule,
     FormsModule,
     HttpModule,
     NgbModule.forRoot()
->>>>>>> 3136ed6838b3d5100829a59b12fce5f00593cbef
   ],
   providers: [],
   bootstrap: [AppComponent]
